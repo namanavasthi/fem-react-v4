3ecd41714b421f29790e415ba23468b7c4798596
Naman Avasthi <naman.avasthi@gmail.com>
2019-04-06 15:49:51 -0700

    intial commit: #1 Pure React

---
+++ src/App.js
@@ -0,0 +1,47 @@
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+class App extends React.Component {
+  handleTitleClick() {
+    alert("You clicked the title!");
+  }
+
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Pet Adoption"
+      ),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockateil"
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        animal: "Cat",
+        breed: "Mixed"
+      })
+    ]);
+  }
+}
+
+// const App = () => {
+//   // Stateful functional component
+// };
+
+ReactDOM.render(
+  React.createElement(App, {}, null),
+  document.getElementById("root")
+);
