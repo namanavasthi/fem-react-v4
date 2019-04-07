ef70b93f43080a5676f3865d230187c6001bec08
Naman Avasthi <naman.avasthi@gmail.com>
2019-04-07 14:31:58 -0700

    #7.1 Async UI Render components from API Data and Remove Class Constructor with Babel

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,63 @@
 import React from "react";
+import pf from "petfinder-client";
+import { navigate } from "@reach/router";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class Details extends React.Component {
+  state = {
+    loading: true
+  };
+
+  componentDidMount() {
+    petfinder.pet
+      .get({
+        output: "full",
+        id: this.props.id
+      })
+      .then(data => {
+        let breed;
+        const pet = data.petfinder.pet;
+
+        if (Array.isArray(pet.breeds.breed)) {
+          breed = pet.breeds.breed.join(", ");
+        } else {
+          breed = pet.breeds.breed;
+        }
+
+        this.setState({
+          name: pet.name,
+          animal: pet.animal,
+          location: `${pet.contact.city}, ${pet.contact.state}`,
+          description: pet.description,
+          media: pet.media,
+          breed,
+          loading: false
+        });
+      })
+      .catch(() => {
+        // this.setState({ error });
+        navigate("/");
+      });
+  }
   render() {
-    return <div>{this.props.id}</div>;
+    if (this.state.loading) {
+      return <h1>Loading</h1>;
+    }
+    const { name, animal, breed, location, description } = this.state;
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>{breed}</h2>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+        <p>{description}</p>
+      </div>
+    );
   }
 }
 
