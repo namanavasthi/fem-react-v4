36a94e310a87241c49924d54811b4c5b2111afd9
Naman Avasthi <naman.avasthi@gmail.com>
2019-04-07 13:58:40 -0700

    #6 Routing

---
+++ src/Results.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Pet from "./Pet";
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+class Results extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Los Angeles, CA" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        // does a shallow merge
+        this.setState({
+          pets
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        {this.state.pets.map(pet => {
+          let breed;
+          if (Array.isArray(pet.breeds.breed)) {
+            breed = pet.breeds.breed.join(", ");
+          } else {
+            breed = pet.breeds.breed;
+          }
+          return (
+            <Pet
+              key={pet.id}
+              id={pet.id}
+              media={pet.media}
+              location={`${pet.contact.city}, ${pet.contact.state}`}
+              animal={pet.animal}
+              name={pet.name}
+              breed={breed}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Results;
