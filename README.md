9c2c43544d2fb60f10cda1aec108df311ee82389
Naman Avasthi <naman.avasthi@gmail.com>
2019-04-07 12:57:27 -0700

    #5 Rendering Data

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This course is given by Brian Holt
 Parcel is a relatively new bundler for JavaScript projects. Webpack is a fantastic piece of technology and you should definitely consider using it for your large applications; it's been around a long time and has a lot of support.
 
 That being said, Parcel is an amazing tool that zero-config. It works with everything we want to do out of the box. Since this is a class on React and not build processes, this allows us to focus more on React.
+
+Parcel reads .env file automatically
--- src/App.js
+++ src/App.js
@@ -1,17 +1,74 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Los Angeles, CA" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        // does a shallow merge
+        this.setState({
+          pets
+        });
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>Pet Adoption</h1>
-        <Pet name="Luna" animal="Dog" breed="Havanese" />
-        <Pet name="Pepper" animal="Bird" breed="Cockateil" />
-        <Pet name="Doink" animal="Cat" breed="Mixed" />
+        {/* <pre>
+          <code>{JSON.stringify(this.state, null, 4)}</code>
+        </pre> */}
+        <div>
+          {this.state.pets.map(pet => {
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                key={pet.id}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+                animal={pet.animal}
+                name={pet.name}
+                breed={breed}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
 }
+
 render(<App />, document.getElementById("root"));
